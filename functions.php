3dc41aad5a264c4340c921baa2c30323696b77fd
Alex Devero <deveroalex@gmail.com>
2018-06-24 11:09:14 +0200

    Reset functions

--- functions.php
+++ functions.php
@@ -6,9 +6,9 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
 if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-  function chld_thm_cfg_parent_css() {
-    wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'font-awesome','owl-theme-default','owl-carousel','perfect-scrollbar' ) );
-  }
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'font-awesome','owl-theme-default','owl-carousel','perfect-scrollbar' ) );
+    }
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
